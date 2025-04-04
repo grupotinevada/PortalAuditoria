3ae759163988a1e7532190ced8bae0c37f2d5759
Mariox20 <marioaguilerav15@gmail.com>
2025-04-04 12:30:39 -0300

    Vista de Procesos por Sociedad

--- src/models/proceso.model.ts
+++ src/models/proceso.model.ts
@@ -1,10 +1,14 @@
 export interface IProceso{
     idproceso: number | null,
     idsociedad: number | null,
+    nombresociedad: string | null,
     nombreproceso: string | null,
     fecha_inicio: Date | null,
-    fecha_termino: Date | null,
+    fecha_fin: Date | null,
     responsable: string | null,
+    responsable_nombre: string | null,
     revisor: string | null,
-    idestado: number | null
+    revisor_nombre: string | null,
+    idestado: number | null,
+    descestado: string | null
 }
